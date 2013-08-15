5a6ead1fc081d089ff388d7cb7d8d75044a9c7fa
Mark Harris <mharris@technobear.com>
2013-08-15 19:42:08 +0200

    2.0 tidy up

--- creator/bin/createsetup.sh
+++ creator/bin/createsetup.sh
@@ -6,7 +6,6 @@ pwd
 DIR=bin
 DIR3=$DIR/thirdparty
 CP=$DIR/com.technobear.jar:$DIR3/xmlrpc-client-3.1.3.jar:$DIR3/xmlrpc-common-3.1.3.jar:$DIR3/ws-commons-util-1.0.2.jar
-echo java -cp $CP com.technobear.eigenharp.creator.CreateLayoutApp "$@" 
 java -cp "$CP" com.technobear.eigenharp.creator.CreateSetupApp "$@" 
 
 
