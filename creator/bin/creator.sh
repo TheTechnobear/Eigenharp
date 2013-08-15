1b19e7909a302fe0009f6966e519684c5eec1e33
Mark Harris <mharris@technobear.com>
2013-08-15 23:40:01 +0200

    oops forgot to change shell script

--- creator/bin/creator.sh
+++ creator/bin/creator.sh
@@ -6,7 +6,7 @@ pwd
 DIR=bin
 DIR3=$DIR/thirdparty
 CP=$DIR/com.technobear.jar:$DIR3/xmlrpc-client-3.1.3.jar:$DIR3/xmlrpc-common-3.1.3.jar:$DIR3/ws-commons-util-1.0.2.jar
-java -cp "$CP" com.technobear.eigenharp.creator.CreateSetupApp "$@" 
+java -cp "$CP" com.technobear.eigenharp.creator.CreatorApp "$@" 
 
 
 
